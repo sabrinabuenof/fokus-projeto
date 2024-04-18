a44ff9315a72281f395a2c83949a8e0dc79b8182
sabrinabuenof <127441722+sabrinabuenof@users.noreply.github.com>
2024-04-17 21:13:38 -0300

    utilizando o innerHTML

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 focoBt.addEventListener('click', () => {
     alterarContexto ('foco');
@@ -20,4 +21,24 @@ longoBt.addEventListener('click', () => {
 function alterarContexto (contexto) {
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada?<strong class="app__title-strong"> Faça um pausa curta!` 
+
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça um pausa longa! 
+            `
+         
+        default:
+            break;
+    }
 }
